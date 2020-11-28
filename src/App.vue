a7fa140fc6c7530b90c39d22508f66cc9090e6fe
Jean-Da-Rocha <jean.darocha@protonmail.com>
2020-11-28 16:19:17 +0100

    feat(app-design): init app design, work in progress

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
+
+<style lang="scss">
+.theme--light.v-application {
+  background-color: var(--v-background-base, #24293d) !important;
+}
+</style>
--- src/views/Player.vue
+++ src/views/Player.vue
@@ -1,5 +1,39 @@
 <template>
-  <div></div>
+  <v-container>
+    <v-row>
+      <v-col class="text-left">
+        <v-btn class="mx-2" fab color="primary">
+          <v-icon>
+            mdi-arrow-left
+          </v-icon>
+        </v-btn>
+      </v-col>
+      <div style="margin-top: 10rem"></div>
+      <v-col class="text-right">
+        <v-btn class="mx-2" fab color="primary">
+          <v-icon>
+            mdi-heart-outline
+          </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="d-flex justify-center" cols="12">
+        <v-img
+          src="https://media.senscritique.com/media/000016786835/source_big/The_Time_Thief.jpg"
+          class="rounded-circle"
+          max-height="250"
+          max-width="250"
+        ></v-img>
+      </v-col>
+      <v-col cols="12" class="text-center">
+        <p class="display-2 white--text" style="text-shadow: #FC0 1px 0 10px;">
+          2048
+        </p>
+        <p class="overline grey--text">Redtenbacher's Funkestra</p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
