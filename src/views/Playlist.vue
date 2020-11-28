bf7b0417df41d01cafbc6268ae668baf19582287
Jean-Da-Rocha <jean.darocha@protonmail.com>
2020-11-28 23:03:32 +0100

    feat(player): add songs data and v-for loop to display song info

--- src/views/Playlist.vue
+++ src/views/Playlist.vue
@@ -9,19 +9,43 @@
     <v-container>
       <v-row dense>
         <v-col cols="12">
-          <v-card dark>
-            <v-card-title class="headline">
-              Unlimited music now
-            </v-card-title>
-            <v-card-subtitle>
-              Listen to your favorite artists and albums whenever and wherever,
-              online and offline.
-            </v-card-subtitle>
-            <v-card-actions>
-              <v-btn text>
-                Listen Now
-              </v-btn>
-            </v-card-actions>
+          <v-card
+            dark
+            shaped
+            v-for="(song, index) in songs"
+            :key="index"
+            class="my-5"
+          >
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title>{{ song.title }}</v-card-title>
+                <v-card-subtitle>
+                  Artiste - {{ song.artist }}
+                  <br />
+                  Genre - {{ song.genre }}
+                </v-card-subtitle>
+                <v-card-text>
+                  {{ song.description }}
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn class="ml-2 mt-5" outlined rounded small>
+                    Écouter
+                  </v-btn>
+                </v-card-actions>
+              </div>
+
+              <v-avatar
+                class="ma-3"
+                size="100"
+                color="primary"
+                style="filter: drop-shadow(0 0 2mm #ee44aa);"
+              >
+                <v-img
+                  class="rounded-circle"
+                  :src="require(`@/assets/${song.coverImage}`)"
+                ></v-img>
+              </v-avatar>
+            </div>
           </v-card>
         </v-col>
       </v-row>
@@ -30,7 +54,14 @@
 </template>
 
 <script>
+import songs from '../data/playlist';
+
 export default {
   name: 'Playlist',
+  data() {
+    return {
+      songs,
+    };
+  },
 };
 </script>
